402bb138393514b5754fc0cef4ac513808a50348
Riley Anderson <10803452@uvu.edu>
2022-09-07 18:20:00 -0600

    added some comment code

--- Matching/Assets/Tools/NewScripts/MonoEventsBehaviour.cs
+++ Matching/Assets/Tools/NewScripts/MonoEventsBehaviour.cs
@@ -6,16 +6,17 @@ public class MonoEventsBehaviour : MonoBehaviour
 {
     public UnityEvent startEvent, awakeEvent, disableEvent;
 
+    //Awake happens before start
     private void Awake()
     {
         awakeEvent.Invoke();
     }
-
+    //Activates when the game starts
     private void Start()
     {
         startEvent.Invoke();
     }
-
+    //Activates when a function becomes disabled
     private void OnDisable()
     {
         disableEvent.Invoke();
