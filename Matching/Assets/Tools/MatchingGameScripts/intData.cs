bac06a757c077bbecad751f5987f6d59a0d269d2
Riley Anderson <10803452@uvu.edu>
2022-09-12 18:03:35 -0600

    linked up score

--- Matching/Assets/Tools/MatchingGameScripts/intData.cs
+++ Matching/Assets/Tools/MatchingGameScripts/intData.cs
@@ -1,8 +1,12 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 [CreateAssetMenu]
 public class intData : ScriptableObject
 {
+    public UnityEvent disableEvent;
     public int value;
 
     public void SetValue(int num)
@@ -13,5 +17,10 @@ public class intData : ScriptableObject
     public void UpdateValue(int num)
     {
         value += num;
+    }
+
+    private void OnDisable()
+    {
+        disableEvent.Invoke();
     }
 }
